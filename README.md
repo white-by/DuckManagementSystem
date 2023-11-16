94253a9c99d267aca75ae1a8324136d2da291784
whiteby <2115182149@qq.com>
2023-11-16 23:30:01 +0800

    update readme.md and sql

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # duckManagementSystem
 
 First of all, thanks for reading.
+首先，感谢阅读。
 
-This project is the final assignment of my Java course in the second year of college.
+---
 
 ## Introduction
 
-This is the final assignment of the Java Fundamentals course that I wrote in my sophomore year.
+This project is the final assignment of my Java course in the second year of college.
 
 A management system related to the school curriculum that is not separated from the front and back
+full name：Sun Yat-sen Management System
 
 ## Tech stack
 
@@ -16,12 +18,64 @@ A management system related to the school curriculum that is not separated from 
 - Database: MySQL
 - Front-end: HTML、CSS and JavaScript, Vue , Bootstrap
 - Backend: Springboot, MyBatis, SpringMVC
+- JDK：jdk1.8
+
+## Please prepare before you get started
+
+-initialize
+
+1. Maven has been configured
+2. The MySQL database has been configured
+   2.1. In the local Mysql, create a database with the database name duck_management_system and import the .sql file I provided.
+3. Change the password to your own MySQL password in the 'application.yml' file
+4. Run
+
+-landing
+
+1. Administrator: admin
+2. Teacher: TEAC
+3. Students: STU
+4. The password is 123
+
+## Implement features
+
+- ps：
+  - The administrator permission level is 1, 2 for teachers and 3 for students
+  - When adding a new user, the initial password is 123.
+- Login screen
+-administrator
+  -landing
+  - Personal interface
+  - Administrator Table
+  - Add, delete, and modify administrator tables
+-teacher
+  -landing
+  - Personal interface
+  - Teacher Table
+  - Additions, deletions and changes to the teacher's list
+-student
+  -landing
+  - Personal interface
+  - Student Sheets
+  - Additions, deletions and changes to the student table
+-course
+  - Full schedule of courses
+  - Permissions management
+- Course enrollment
+  - Student course selection
+  - Permissions management
+  - The course enrollment limit has been reached
+  - Duplicate course selection questions
+- Return to the login screen
+
+---
 
 ## 简介
 
 这是本人于大二编写的Java基础课程的期末大作业。
 
 一个前后端不分离的与学校课程相关的管理系统
+全名：双鸭山管理系统
 
 ## 技术栈
 
@@ -29,3 +83,52 @@ A management system related to the school curriculum that is not separated from 
 - 数据库：MySQL
 - 前端：前端三件套、vue框架、bootstrap模板
 - 后端：Springboot、MyBatis、SpringMVC
+- JDK版本：jdk1.8
+
+## 上手前请准备
+
+- 初始化
+
+1. 已经配置maven
+2. 已经配置mysql数据库
+   2.1. 在本地的Mysql中，创建一个数据库名称为 duck_management_system 的数据库，并导入我提供的 .sql 文件,
+3. 在`application.yml`文件中将密码改成自己mysql的密码
+4. 运行
+
+- 登陆
+
+1. 管理员：admin
+2. 教师：teac
+3. 学生：stu
+4. 密码均为123
+
+## 实现功能
+
+- ps：
+  - 管理员权限等级为1，教师2，学生3
+  - 新增用户时，初始密码“123”
+- 登陆界面
+- 管理员
+  - 登陆
+  - 个人界面
+  - 管理员表
+  - 管理员表增删改
+- 教师
+  - 登陆
+  - 个人界面
+  - 教师表
+  - 教师表增删改
+- 学生
+  - 登陆
+  - 个人界面
+  - 学生表
+  - 学生表增删改
+- 课程
+  - 课程全表
+  - 权限管理
+- 选课
+  - 学生选课
+  - 权限管理
+  - 选课达到上限
+  - 重复选课问题
+- 退回登陆界面
---
+++ duck_management_system.sql
@@ -0,0 +1,154 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80026
+ Source Host           : localhost:3306
+ Source Schema         : duck_management_system
+
+ Target Server Type    : MySQL
+ Target Server Version : 80026
+ File Encoding         : 65001
+
+ Date: 16/11/2023 23:28:42
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin_info
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_info`;
+CREATE TABLE `admin_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户名字',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
+  `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '性别',
+  `age` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '年龄',
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '手机号',
+  `level` int NULL DEFAULT 1 COMMENT '权限等级',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 41 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin_info
+-- ----------------------------
+INSERT INTO `admin_info` VALUES (1, 'admin', '123', '女', '36', '13680524090', 1);
+INSERT INTO `admin_info` VALUES (2, 'Tom', '123', '男', '27', '17566889723', 1);
+INSERT INTO `admin_info` VALUES (3, 'Jerry', '123', '女', '31', '18929010106', 1);
+INSERT INTO `admin_info` VALUES (38, 'Mancy', '123', '女', '23', '18860200752', 1);
+INSERT INTO `admin_info` VALUES (40, 'duck', '123', '女', NULL, NULL, 1);
+
+-- ----------------------------
+-- Table structure for class_info
+-- ----------------------------
+DROP TABLE IF EXISTS `class_info`;
+CREATE TABLE `class_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '课程名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '结课方式',
+  `major` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '开设学院',
+  `score` int NULL DEFAULT 0 COMMENT '学分',
+  `teacherId` bigint NULL DEFAULT NULL COMMENT '教师ID',
+  `classOpen` int NULL DEFAULT NULL COMMENT '人数上限',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '上课时间',
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '上课地点',
+  `classChoose` int NULL DEFAULT 0 COMMENT '已选课程人数',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of class_info
+-- ----------------------------
+INSERT INTO `class_info` VALUES (1, '高等数学', '考试', '数学学院', 5, 1, 3, '周一1-2', 'if楼305', 2);
+INSERT INTO `class_info` VALUES (5, 'Java基础', '考核', '软件工程', 2, 2, 60, '周二7-8', 'B203', 3);
+INSERT INTO `class_info` VALUES (6, '线性代数', '考试', '软件工程', 3, 5, 45, '周四3-4', '丰盛堂C307', 1);
+INSERT INTO `class_info` VALUES (7, '中国近代史', '考试', '历史学院', 3, 2, 50, '周三5-6', '丰盛堂A105', 0);
+INSERT INTO `class_info` VALUES (9, '操作系统', '考试', '计算机学院', 3, 7, 30, '周五3-4', 'C503', 0);
+INSERT INTO `class_info` VALUES (13, '学术制图', '考核', '化学学院', 2, 6, 10, '周二-5-6', 'D201', 1);
+
+-- ----------------------------
+-- Table structure for course_select_info
+-- ----------------------------
+DROP TABLE IF EXISTS `course_select_info`;
+CREATE TABLE `course_select_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '课程名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '结课方式',
+  `major` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '开设学院',
+  `score` int NULL DEFAULT 0 COMMENT '学分',
+  `teacherId` bigint NULL DEFAULT NULL COMMENT '教师ID',
+  `classOpen` int NULL DEFAULT 0 COMMENT '人数上限',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '上课时间',
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '上课地点',
+  `classChoose` int NULL DEFAULT 0 COMMENT '课程已选人数',
+  `studentId` bigint NULL DEFAULT NULL COMMENT '选课学生ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of course_select_info
+-- ----------------------------
+INSERT INTO `course_select_info` VALUES (9, '高等数学', '考试', '数学学院', 5, 1, 1, '周一1-2', 'if楼305', 0, 1);
+INSERT INTO `course_select_info` VALUES (10, '线性代数', '考试', '软件工程', 3, 5, 50, '周四3-4', '丰盛堂C307', 0, 1);
+INSERT INTO `course_select_info` VALUES (11, 'Java基础', '考核', '软件工程', 2, 2, 60, '周二7-8', '教学大楼B203', 0, 2);
+INSERT INTO `course_select_info` VALUES (12, '高等数学', '考试', '数学学院', 5, 1, 2, '周一1-2', 'if楼305', 1, 6);
+INSERT INTO `course_select_info` VALUES (13, 'Java基础', '考核', '软件工程', 2, 2, 60, '周二7-8', '教学大楼B203', 1, 6);
+INSERT INTO `course_select_info` VALUES (14, 'Java基础', '考核', '软件工程', 2, 2, 60, '周二7-8', '教学大楼B203', 2, 1);
+INSERT INTO `course_select_info` VALUES (15, '学术制图', '考核', '化学学院', 2, 6, 10, '周二-5-6', 'D201', 0, 5);
+
+-- ----------------------------
+-- Table structure for student_info
+-- ----------------------------
+DROP TABLE IF EXISTS `student_info`;
+CREATE TABLE `student_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户名字',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
+  `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '性别',
+  `age` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '年龄',
+  `uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '学号',
+  `level` int NULL DEFAULT 3 COMMENT '权限等级',
+  `major` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '学院',
+  `score` int NULL DEFAULT NULL COMMENT '学分',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student_info
+-- ----------------------------
+INSERT INTO `student_info` VALUES (1, 'stu', '123', '女', '19', '22331001', 3, '软件工程', 55);
+INSERT INTO `student_info` VALUES (2, 'student', '123', '女', '21', '21331009', 3, '计算机科学与技术', 89);
+INSERT INTO `student_info` VALUES (3, 'Wattit', '123', '女', '20', '21331080', 3, '人文社科', 10);
+INSERT INTO `student_info` VALUES (5, 'Duo', '123', '男', '23', '19011127', 3, '生物工程', 127);
+INSERT INTO `student_info` VALUES (6, 'Adam', '123', '男', '20', '21131674', 3, '数学系', 70);
+INSERT INTO `student_info` VALUES (7, 'Chris', '123', '女', '19', '22261005', 3, 'AI', 53);
+
+-- ----------------------------
+-- Table structure for teacher_info
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher_info`;
+CREATE TABLE `teacher_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户名字',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
+  `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '性别',
+  `age` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '年龄',
+  `level` int NULL DEFAULT 2 COMMENT '权限等级',
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '职称',
+  `major` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '学院',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teacher_info
+-- ----------------------------
+INSERT INTO `teacher_info` VALUES (1, 'teac', '123', '女', '38', 2, '教授', '软件工程');
+INSERT INTO `teacher_info` VALUES (2, 'teacher', '123', '女', '45', 2, '讲师', 'AI');
+INSERT INTO `teacher_info` VALUES (5, 'teacherZhang', '123', '男', '48', 2, '讲师', '微电子');
+INSERT INTO `teacher_info` VALUES (6, 'teacherWang', '123', '女', '41', 2, '副教授', '历史类');
+INSERT INTO `teacher_info` VALUES (7, 'teacherZhao', '123', '男', '52', 2, '副教授', '数学系');
+
+SET FOREIGN_KEY_CHECKS = 1;
